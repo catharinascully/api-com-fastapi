cfbcce23991b03015c89dd3a8ab3ad9a24a22baf
catharinascully <catharina.scully@gmail.com>
2025-05-05 16:48:51 -0300

    commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from model.database import Database
 
 app = FastAPI()
 
-@app.get('/')
-def read_hello():
-    return {"Hello" : "World"}
+db = Database()
+serie_db = [] # Lista que simula um banco de dados
 
-@app.get('/items/{item_id}/{query}')
-def read_item(item_id: int, query: Union[str, None] = None):
-    return {"item_id": item_id, "query": query}
+class Serie(BaseModel):
+    id: str
+    titulo: str
+    descricao: str
+    ano_lancamento: int
+    id_categoria: int
+
+@app.post('/series/')
+def cadastrar(serie: Serie):
+    db.conectar()
+    sql = "INSERT INTO serie (titulo, descricao, ano_lancamento, id_categoria) VALUES (%s, %s,%s,%s)"
+    db.executar(sql,(serie.titulo, serie.descricao,serie.ano_lancamento, serie.id_categoria))
+    db.desconectar()
+    return {"mensagem": "Série cadastrada com sucesso", "serie": serie}
+ 
+ 
--- model/database.py
+++ model/database.py
@@ -1,19 +1,20 @@
 import mysql.connector as mc # Importando a biblioteca do conector do MySQL
-from mysql.connector import Error # Importando a classe Error para tratar as mensagens de erro do código
+from mysql.connector import Error, MySQLConnection # Importando a classe Error para tratar as mensagens de erro do código
 from dotenv import load_dotenv # Importando a função load_dotenv
 from os import getenv
+from typing import Optional, Any, Tuple, List
 
 class Database:
-    def __init__(self):
+    def __init__(self) -> None:
         load_dotenv()
-        self.host = getenv('DB_HOST')
-        self.username = getenv('DB_USER')
-        self.password = getenv('BD_PSWD')
-        self.database = getenv('DB_DATABASE')
-        self.connection = None # Inicialização da conexão
-        self.cursor = None # Inicialização do cursor
+        self.host: str = getenv('DB_HOST')
+        self.username: str = getenv('DB_USER')
+        self.password: str = getenv('BD_PSWD')
+        self.database: str = getenv('DB_DATABASE')
+        self.connection: Optional[MySQLConnection] = None # Inicialização da conexão
+        self.cursor: Optional[List[dict]] = None # Inicialização do cursor
 
-    def conectar(self):
+    def conectar(self) -> None:
         """Estabelece uma conexão com o banco de dados."""
 
         try: 
@@ -31,7 +32,7 @@ class Database:
             self.connection = None
             self.cursor = None
 
-    def desconectar(self):
+    def desconectar(self) -> None:
         """Encerra a conexão com o banco de dados e o cursor, se eles existirem"""
         if self.cursor:
             self.cursor.close()
@@ -39,7 +40,7 @@ class Database:
             self.connection.close()
         print("Conexão com o banco de dados encerrada com sucesso!")
 
-    def executar(self, sql, params = None):
+    def executar(self, sql: str, params: Optional[Tuple[Any, ...]] = None, fetch: bool = False) -> Optional[List[dict]]:
         """Executa uma instrução no banco de dados."""
         if self.connection is None and self.cursor is None:
             print("Conexão ao banco de dados não estabelecida.")
@@ -47,22 +48,12 @@ class Database:
 
         try: 
             self.cursor.execute(sql, params)
-            self.connection.commit()
-            return self.cursor
+            if fetch:
+                self.cursor.fetchall()
+            else:
+                self.connection.commit()
+                return self.cursor
         except Error as e:
             print(f"Erro de conexão: {e}")
             return None
-        
-    
-    def consultar(self, sql, params = None):
-        """Executa uma consulta no banco de dados."""
-        if self.connection is None and self.cursor is None:
-            print("Conexão ao banco de dados não estabelecida.")
-            return None 
 
-        try: 
-            self.cursor.execute(sql, params)
-            return self.cursor.fetchall()
-        except Error as e:
-            print(f"Erro de conexão: {e}")
-            return None
